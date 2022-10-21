df68cd2753b98da748f9fd3befe11c179658092f
Winston Puckett <puckett.bobby7@gmail.com>
2022-10-21 23:21:55 +0000

    Always show exit code

--- sourceable/prompt.sh
+++ sourceable/prompt.sh
@@ -1,11 +1,6 @@
-function print_exit_code() {
-    EXIT_CODE=$?
-    [ $EXIT_CODE -ne 0 ] && echo " | $EXIT_CODE"
-}
-
 function print_branch_name() {
     BRANCH=$(git branch --show-current 2>/dev/null)
     [ "$BRANCH" ] && echo " | $BRANCH"
 }
 
-export PS1="\n\[\e[35m\]\w\`print_exit_code\`\`print_branch_name\`\[\e[m\]\n\[\e[35m\]❯\[\e[m\] "
+export PS1="\n\[\e[35m\]\w | $?\`print_branch_name\`\[\e[m\]\n\[\e[35m\]❯\[\e[m\] "
